ff87c07cceec5622264815cb57f2f1a4f4df9f79
abdulbasit-png <beapunjabian12@gmail.com>
2024-12-27 19:17:51 +0500

    Updated JavaScript code to improve mobile compatibility

--- js/vlt-helpers.js
+++ js/vlt-helpers.js
@@ -1,145 +1,124 @@
-/***********************************************
- * VLThemes
- ***********************************************/
-
-'use strict';
+"use strict";
 
 /**
  * Initialize main helper object
  */
 var VLTJS = {
-	window: jQuery(window),
-	document: jQuery(document),
-	html: jQuery('html'),
-	body: jQuery('body'),
-	is_safari: /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
-	is_firefox: navigator.userAgent.toLowerCase().indexOf('firefox') > -1,
-	is_chrome: /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor),
-	is_ie10: navigator.appVersion.indexOf('MSIE 10') !== -1,
-	transitionEnd: 'transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd',
-	animIteration: 'animationiteration webkitAnimationIteration oAnimationIteration MSAnimationIteration',
-	animationEnd: 'animationend webkitAnimationEnd'
+  window: jQuery(window),
+  document: jQuery(document),
+  html: jQuery("html"),
+  body: jQuery("body"),
+  is_safari: /^((?!chrome|android).)*safari/i.test(navigator.vendor),
+  is_firefox: navigator.userAgent.toLowerCase().indexOf("firefox") > -1,
+  is_chrome:
+    /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor),
+  is_ie10: navigator.appVersion.indexOf("MSIE 10") !== -1,
+  transitionEnd:
+    "transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd",
+  animIteration:
+    "animationiteration webkitAnimationIteration oAnimationIteration MSAnimationIteration",
+  animationEnd: "animationend webkitAnimationEnd",
 };
 
 /**
  * Detects whether user is viewing site from a mobile device
  */
 VLTJS.isMobile = {
-	Android: function () {
-		return navigator.userAgent.match(/Android/i);
-	},
-	BlackBerry: function () {
-		return navigator.userAgent.match(/BlackBerry/i);
-	},
-	iOS: function () {
-		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	},
-	Opera: function () {
-		return navigator.userAgent.match(/Opera Mini/i);
-	},
-	Windows: function () {
-		return navigator.userAgent.match(/IEMobile/i);
-	},
-	any: function () {
-		return (VLTJS.isMobile.Android() || VLTJS.isMobile.BlackBerry() || VLTJS.isMobile.iOS() || VLTJS.isMobile.Opera() || VLTJS.isMobile.Windows());
-	}
+  any: function () {
+    return /Mobi|Android/i.test(navigator.userAgent);
+  },
 };
 
 /**
  * Debounce resize
  */
-var resizeArr = [];
-var resizeTimeout;
-VLTJS.window.on('load resize orientationchange', function(e) {
-	if (resizeArr.length) {
-		clearTimeout(resizeTimeout);
-		resizeTimeout = setTimeout(function() {
-			for (var i = 0; i < resizeArr.length; i++) {
-				resizeArr[i](e);
-			}
-		}, 250);
-	}
-});
+var debounceResize = _.debounce(function (e) {
+  if (resizeArr.length) {
+    for (var i = 0; i < resizeArr.length; i++) {
+      resizeArr[i](e);
+    }
+  }
+}, 250);
 
-VLTJS.debounceResize = function(callback) {
-	if (typeof callback === 'function') {
-		resizeArr.push(callback);
-	} else {
-		window.dispatchEvent(new Event('resize'));
-	}
-}
+var resizeArr = [];
+VLTJS.window.on("resize orientationchange", debounceResize);
+
+VLTJS.debounceResize = function (callback) {
+  if (typeof callback === "function") {
+    resizeArr.push(callback);
+  } else {
+    window.dispatchEvent(new Event("resize"));
+  }
+};
 
 VLTJS.addLedingZero = function (number) {
-	return ('0' + number).slice(-2);
-}
+  return ("0" + number).slice(-2);
+};
 
 /**
  * Throttle scroll
  */
-var throttleArr = [];
-var didScroll;
-var delta = 5;
-var lastScrollTop = 0;
+var throttleScroll = _.throttle(function () {
+  if (throttleArr.length) {
+    var scrollTop = VLTJS.window.scrollTop(),
+      windowHeight = VLTJS.window.height(),
+      documentHeight = VLTJS.document.height(),
+      scrollState = "";
 
-VLTJS.window.on('load resize scroll orientationchange', function() {
-	if (throttleArr.length) {
-		didScroll = true;
-	}
-});
+    if (Math.abs(lastScrollTop - scrollTop) > delta) {
+      if (scrollTop > lastScrollTop) {
+        scrollState = "down";
+      } else if (scrollTop < lastScrollTop) {
+        scrollState = "up";
+      } else {
+        scrollState = "none";
+      }
 
-function hasScrolled() {
+      if (scrollTop === 0) {
+        scrollState = "start";
+      } else if (scrollTop >= documentHeight - windowHeight) {
+        scrollState = "end";
+      }
 
-	var scrollTop = VLTJS.window.scrollTop(),
-		windowHeight = VLTJS.window.height(),
-		documentHeight = VLTJS.document.height(),
-		scrollState = '';
+      for (var i in throttleArr) {
+        if (typeof throttleArr[i] === "function") {
+          throttleArr[i](scrollState, scrollTop, lastScrollTop, VLTJS.window);
+        }
+      }
 
-	// Make sure they scroll more than delta
-	if (Math.abs(lastScrollTop - scrollTop) <= delta) {
-		return;
-	}
-
-	if (scrollTop > lastScrollTop) {
-		scrollState = 'down';
-	} else if (scrollTop < lastScrollTop) {
-		scrollState = 'up';
-	} else {
-		scrollState = 'none';
-	}
-
-	if (scrollTop === 0) {
-		scrollState = 'start';
-	} else if (scrollTop >= documentHeight - windowHeight) {
-		scrollState = 'end';
-	}
-
-	for (var i in throttleArr) {
-		if (typeof throttleArr[i] === 'function') {
-			throttleArr[i](scrollState, scrollTop, lastScrollTop, VLTJS.window);
-		}
-	}
-
-	lastScrollTop = scrollTop;
-}
-
-setInterval(function() {
-	if (didScroll) {
-		didScroll = false;
-		window.requestAnimationFrame(hasScrolled);
-	}
+      lastScrollTop = scrollTop;
+    }
+  }
 }, 250);
 
-VLTJS.throttleScroll = function(callback) {
-	if (typeof callback === 'function') {
-		throttleArr.push(callback);
-	}
-}
+var throttleArr = [];
+var delta = 5;
+var lastScrollTop = 0;
+VLTJS.window.on("scroll", throttleScroll);
+
+VLTJS.throttleScroll = function (callback) {
+  if (typeof callback === "function") {
+    throttleArr.push(callback);
+  }
+};
 
 /**
  * VAR polyfill
  */
-if (typeof cssVars !== 'undefined') {
-	cssVars({
-		onlyVars: true,
-	});
+if (!("CSS" in window && CSS.supports("color", "var(--fake-var)"))) {
+  if (typeof cssVars !== "undefined") {
+    cssVars({
+      onlyVars: true,
+    });
+  }
 }
+
+/**
+ * RequestAnimationFrame polyfill
+ */
+window.requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  function (callback) {
+    return setTimeout(callback, 1000 / 60);
+  };
